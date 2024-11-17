4a23a43e96ecb5af5d89002c415104f3eaaaffa9
almakagan <damirhanalmakhan@gmail.com>
2024-11-17 12:37:42 +0500

    initial commit

---
+++ src/main/java/com/example/smartmicrovawe/SmartMicrovaweApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartmicrovawe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartMicrovaweApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartMicrovaweApplication.class, args);
+    }
+
+}
